8716b0ebf612b63d3e0dd4ddb3b1e6aa6fce8eed
Craig Booker <craig@arcadiasoftworks.com>
2020-02-26 00:29:55 -0600

    Made some visual css changes to the navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,9 +61,7 @@ const Navbar = class extends React.Component {
 							<Link className='navbar-item' to='/about'>
 								About
 							</Link>
-							<Link className='navbar-item' to='/apps'>
-								Apps
-							</Link>
+
 							<Link className='navbar-item' to='/blog'>
 								Blog
 							</Link>
