cac30f7222ac391829c2a1d45e8fd08642846599
Craig Booker <craig@arcadiasoftworks.com>
2020-02-26 15:33:08 -0600

    Minor change to blog hero image and to the netlify toml file.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import BlogRoll from '../../components/BlogRoll'
+import React from 'react';
+import Layout from '../../components/Layout';
+import BlogRoll from '../../components/BlogRoll';
 
 export default class BlogIndexPage extends React.Component {
 	render() {
 		return (
 			<Layout>
 				<div
-					className="full-width-image-container margin-top-0"
+					className='full-width-image-container margin-top-0'
 					style={{
-						backgroundImage: `url('/img/blog-index.jpg')`
+						backgroundImage: `url('/img/helloWorld1.jpg')`
 					}}
 				>
 					<h1
-						className="has-text-weight-bold is-size-1"
+						className='has-text-weight-bold is-size-1'
 						style={{
 							boxShadow:
 								'0.5rem 0 0 rgb(0, 186, 187, 0.75), -0.5rem 0 0 rgb(0, 186, 187, 0.75)',
@@ -25,14 +25,14 @@ export default class BlogIndexPage extends React.Component {
 						Latest Stories
 					</h1>
 				</div>
-				<section className="section">
-					<div className="container">
-						<div className="content">
+				<section className='section'>
+					<div className='container'>
+						<div className='content'>
 							<BlogRoll />
 						</div>
 					</div>
 				</section>
 			</Layout>
-		)
+		);
 	}
 }
